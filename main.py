4c098595f6a4bc6c68354cf43453d9554e06dff7
Nico Widmer <willyfisch@yahoo.com>
2022-06-28 20:10:06 +0200

    Reformat to object-oriented

--- main.py
+++ main.py
@@ -5,66 +5,105 @@ import json
 import codecs
 import re
 
-source_file_string = input("Enter full path of file, which needs to be translated: ")
-# directory = os.path.dirname(source_file_string)
-file_name = os.path.splitext(source_file_string)[0]
-#file_extension = os.path.splitext(source_file_string)[1]
+file_name = ""
 
-source_file = io.open(source_file_string, mode="r", encoding="utf-8")
-source_content = source_file.read()
 
-# Don't translate music information (description)
-content = source_content.partition("🎵")[0]
-music_content = source_content.partition("🎵")[1] + source_content.partition("🎵")[2]
+def get_input_text():
+    source_file_string = input("Enter full path of file, which needs to be translated: ")
+    # directory = os.path.dirname(source_file_string)
+    global file_name
+    file_name = os.path.splitext(source_file_string)[0]
+    # file_extension = os.path.splitext(source_file_string)[1]
 
-#print(content)
+    source_file = io.open(source_file_string, mode="r", encoding="utf-8")
+    source_content = source_file.read()
 
-# Remove line breaks within sentences (translation)
-timestamp_regex = "0:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9],0:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9]"
-timestamp_location = re.search(timestamp_regex, content)
-if timestamp_location:
-    print(timestamp_location)
-    content = content.replace("\n", " ")      # Remove all new lines and replace with empty string
-    content = re.sub(r"(" + timestamp_regex + " " + ")(.)", r"\1\n\2", content)   # Add back new line after each timestamp
-    content = re.sub(r"(" + timestamp_regex + " " + ")", r"\n\n\1", content)      # Add back two new lines before each timestamp
-    content = content.lstrip("\n")
+    return source_content
 
-print(content)
 
-url = "https://api-free.Translate.com/v2/translate"
-auth_key = "8ce20a2c-0e86-4bba-6773-3be52ea7416e:fx"
+class Translator:
+    music_content = ""
 
-languages = ["DE", "ES", "FR", "IT", "JA", "PT-BR", "TR"]
-for language in languages:
-    if language == "JA" or language == "TR":
-        data = {
-            "auth_key": auth_key,
-            "target_lang": language,
-            "text": content
-        }
-    else:
-        data = {
-            "auth_key": auth_key,
-            "formality": "less",
-            "target_lang": language,
-            "text": content
-        }
+    def extract_music_text(self, text):
+        # Don't translate music information (description)
+        self.music_content = text.partition("🎵")[1] + text.partition("🎵")[2]
 
-    request = requests.post(url=url, data=data)
-    translation = json.loads(request.text)["translations"][0]["text"]
+    def format_input_text(self, text):
+        # Don't translate music information (description)
+        text = text.partition("🎵")[0]
 
-    # No SS!
-    if language == "DE":
-        translation = translation.replace("ß", "ss")
+        # Remove line breaks within sentences (translation)
+        timestamp_regex = "0:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9],0:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9]"
+        timestamp_location = re.search(timestamp_regex, text)
+        if timestamp_location:
+            print(timestamp_location)
+            text = text.replace("\n", " ")  # Remove all new lines and replace with empty string
+            text = re.sub(r"(" + timestamp_regex + " " + ")(.)", r"\1\n\2",
+                          text)  # Add back new line after each timestamp
+            text = re.sub(r"(" + timestamp_regex + " " + ")", r"\n\n\1",
+                          text)  # Add back two new lines before each timestamp
+            text = text.lstrip("\n")
 
-    # Append music information after translating (description)
-    translation = translation + music_content
+        return text
 
-    print(translation)
+    def translate_to_language(self, text, language):
+        url = "https://api-free.deepl.com/v2/translate"
+        auth_key = "8ce20a2c-0e86-4bba-6773-3be52ea7416e:fx"
+        if language == "JA" or language == "TR" or language == "ZH":
+            data = {
+                "auth_key": auth_key,
+                "target_lang": language,
+                "text": text
+            }
+        else:
+            data = {
+                "auth_key": auth_key,
+                "formality": "less",
+                "target_lang": language,
+                "text": text
+            }
+        request = requests.post(url=url, data=data)
+        translation = json.loads(request.text)["translations"][0]["text"]
 
-    translated_file_path = file_name + language + ".txt"
-    if os.path.exists(translated_file_path):
-        os.remove(translated_file_path)
-    translated_file = codecs.open(translated_file_path, "w", "utf-8")
-    translated_file.write(translation)
-    translated_file.close()
+        return translation
+
+    def format_translated_text(self, text, language):
+        # No SS!
+        if language == "DE":
+            text = text.replace("ß", "ss")
+
+        # Append music information after translating (description)
+        text = text + self.music_content
+
+        return text
+
+    def write_to_file(self, text, language):
+        translated_file_path = file_name + language + ".txt"
+        if os.path.exists(translated_file_path):
+            os.remove(translated_file_path)
+        translated_file = codecs.open(translated_file_path, "w", "utf-8")
+        translated_file.write(text)
+        translated_file.close()
+
+
+def main():
+    translator = Translator()
+    unformatted_text = get_input_text()
+    translator.extract_music_text(unformatted_text)
+    formatted_text = translator.format_input_text(unformatted_text)
+    print(formatted_text)
+
+    languages = ["DE", "ES", "FR", "IT", "JA", "PT-BR", "TR", "ZH"]
+    for language in languages:
+        translated_text = translator.translate_to_language(formatted_text, language)
+        translated_text = translator.format_translated_text(translated_text, language)
+
+        print(translated_text)
+
+        translator.write_to_file(translated_text, language)
+
+
+
+
+if __name__ == "__main__":
+    main()
