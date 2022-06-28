f76842942b109762251e18610bc6ddbb7fbf3e84
Nico Widmer <willyfisch@yahoo.com>
2022-06-28 20:40:56 +0200

    Add TextInput class

--- main.py
+++ main.py
@@ -5,30 +5,39 @@ import json
 import codecs
 import re
 
-file_name = ""
 
+class TextInput:
+    directory = ""
+    file_name = ""
+    file_extension = ""
+    text = ""
 
-def get_input_text():
-    source_file_string = input("Enter full path of file, which needs to be translated: ")
-    # directory = os.path.dirname(source_file_string)
-    global file_name
-    file_name = os.path.splitext(source_file_string)[0]
-    # file_extension = os.path.splitext(source_file_string)[1]
+    def get_input_text(self):
+        source_file_string = input("Enter full path of file, which needs to be translated: ")
+        self.directory = os.path.dirname(source_file_string)
+        self.file_name = os.path.splitext(source_file_string)[0]
+        self.file_extension = os.path.splitext(source_file_string)[1]
 
-    source_file = io.open(source_file_string, mode="r", encoding="utf-8")
-    source_content = source_file.read()
-
-    return source_content
+        source_file = io.open(source_file_string, mode="r", encoding="utf-8")
+        self.text = source_file.read()
 
 
 class Translator:
+    url = "https://api-free.deepl.com/v2/translate"
+    auth_key = "8ce20a2c-0e86-4bba-6773-3be52ea7416e:fx"
+
     music_content = ""
+    file_name = ""
+
+    def __init__(self, file_name):
+        self.file_name = file_name
 
     def extract_music_text(self, text):
         # Don't translate music information (description)
         self.music_content = text.partition("🎵")[1] + text.partition("🎵")[2]
 
-    def format_input_text(self, text):
+    @staticmethod
+    def format_input_text(text):
         # Don't translate music information (description)
         text = text.partition("🎵")[0]
 
@@ -47,22 +56,20 @@ class Translator:
         return text
 
     def translate_to_language(self, text, language):
-        url = "https://api-free.deepl.com/v2/translate"
-        auth_key = "8ce20a2c-0e86-4bba-6773-3be52ea7416e:fx"
         if language == "JA" or language == "TR" or language == "ZH":
             data = {
-                "auth_key": auth_key,
+                "auth_key": self.auth_key,
                 "target_lang": language,
                 "text": text
             }
         else:
             data = {
-                "auth_key": auth_key,
+                "auth_key": self.auth_key,
                 "formality": "less",
                 "target_lang": language,
                 "text": text
             }
-        request = requests.post(url=url, data=data)
+        request = requests.post(url = self.url, data = data)
         translation = json.loads(request.text)["translations"][0]["text"]
 
         return translation
@@ -78,7 +85,7 @@ class Translator:
         return text
 
     def write_to_file(self, text, language):
-        translated_file_path = file_name + language + ".txt"
+        translated_file_path = self.file_name + language + ".txt"
         if os.path.exists(translated_file_path):
             os.remove(translated_file_path)
         translated_file = codecs.open(translated_file_path, "w", "utf-8")
@@ -87,10 +94,13 @@ class Translator:
 
 
 def main():
-    translator = Translator()
-    unformatted_text = get_input_text()
-    translator.extract_music_text(unformatted_text)
-    formatted_text = translator.format_input_text(unformatted_text)
+    text_input = TextInput()
+    text_input.get_input_text()
+
+    translator = Translator(text_input.file_name)
+    translator.extract_music_text(text_input.text)
+    formatted_text = translator.format_input_text(text_input.text)
+
     print(formatted_text)
 
     languages = ["DE", "ES", "FR", "IT", "JA", "PT-BR", "TR", "ZH"]
@@ -101,8 +111,6 @@ def main():
         print(translated_text)
 
         translator.write_to_file(translated_text, language)
-
-
 
 
 if __name__ == "__main__":
