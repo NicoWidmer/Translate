57a8e3bda08eabd8115c0ba0b08b0908b631de50
samarawidmer <samarawidmer@gmail.com>
2023-07-08 17:35:30 +0200

    Update test_translate.py

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -3,6 +3,13 @@ import pytest
 from translate import FileHandler
 from translate import Translator
 
+
 @pytest.fixture
 def file_handler():
     return FileHandler()
+
+
+@pytest.fixture
+def mock_config_ini_file(tmp_path):
+    mock_file = tmp_path / "mock_config.ini"
+    mock_file.write.text("Mocked content")
