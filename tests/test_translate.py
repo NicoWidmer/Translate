b0652751b4298e2323ce6aeaac76decc37d5da0c
samarawidmer <samarawidmer@gmail.com>
2023-07-08 19:06:40 +0200

    Created read_config_file_test function

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -10,6 +10,51 @@ def file_handler():
 
 
 @pytest.fixture
-def mock_config_ini_file(tmp_path):
-    mock_file = tmp_path / "mock_config.ini"
-    mock_file.write.text("Mocked content")
+# file with all valid languages
+def mock_config_ini_file():
+    file_content = ";; Add more entries to support additional languages to upload\n" \
+                   ";; Left: Language code in file name (DeepL language code)\n" \
+                   ";; Right: Youtube language code\n" \
+                   "\n" \
+                   "[LANGUAGES]\n" \
+                   "bg = bg\n" \
+                   "cs = cs\n" \
+                   "de = de\n" \
+                   "el = el\n" \
+                   "es = es\n" \
+                   "fi = fi\n" \
+                   "fr = fr\n" \
+                   "hu = hu\n" \
+                   "id = id\n" \
+                   "it = it\n" \
+                   "ja = ja\n" \
+                   "ko = ko\n" \
+                   "nb = no\n" \
+                   "nl = nl\n" \
+                   "pl = pl\n" \
+                   "pt - br = pt - Br\n" \
+                   "ro = ro\n" \
+                   "ru = ru\n" \
+                   "sv = sv\n" \
+                   "tr = tr\n" \
+                   "uk = uk\n" \
+                   "zh = zh - Hans\n" \
+                   "\n" \
+                   "; Nothing will get translated after this character\n" \
+                   "[PROPERTIES]\n" \
+                   "end_translation_character = 🎵\n"
+
+    return file_content
+
+
+def read_config_file_test(ini_file=mock_config_ini_file()):
+    file_handler = FileHandler()
+    file_handler.read_config_file(ini_file)
+    languages_correct = True
+
+    for language in file_handler.languages:
+        if language not in ini_file:
+            languages_correct = False
+            break
+
+    assert languages_correct
